ee0b15a79a1841a0b1c850d0f5495dbd686aba6b
sammyskaratedojo <sammyskaratedojoding@gmail.com>
2026-07-17 11:39:23 +0200

    Revise contents array with new combinations
    Updated contents array with new titles and combinations.

--- script.js
+++ script.js
@@ -5,6 +5,49 @@ const contents = [
     { title: "Rot", combinations: ["1. ZK irgenidasdasd", "2. ZK irgendwatt Zyui-ak"], currentCombination: 0 },
 ]
 
+const contents = [
+    {
+        "title": "5. Kyu (Blau I)",
+        "combinations": [
+            "1. ZK ->: Kizami-Zuki (YA) + vor Oi-Zuki (Kamae)",
+            "2. ZK ->: Gyaku-Zuki (im Stand) + vor Gyaku-Zuki",
+            "3. ZK <-: Age-Uke + Gyaku-Age-Empi",
+            "4. ZK <-: Gedan-Barai + Gyaku-Mawashi-Empi",
+            "5. KK <-: Morote-Uchi-Ude-Uke + ZK Gyaku-Zuki",
+            "6. KK ->: Shuto-Uke + ZK Gyaku-Shuto-Uchi Jodan",
+            "7. ZK ->: Mae-Ashi-Geri + vor Mawashi-Geri (Kamae)",
+            "8. ZK <-: Ushiro-Geri im Rückswärtsgehen ohne Drehung"
+        ],
+        "currentCombination": 0
+    },
+    {
+        "title": "3. Kyu (Braun I)",
+        "combinations": [
+            "1. ZK ->: Oi-Zuki + rückwärts Gedan-Barai + vor Oi-Zuki",
+            "2. KK ->: Gyaku-Zuki + Kizami-Zuki (im Stand) + vor Gyaku-Zuki",
+            "3. KK <-: Age-Uke + Mae-Ashi-Geri + ZK Gyaku-Zuki",
+            "4. ZK ->: Uchi-Ude-Uke + Gyaku-Zuki + Kizami-Zuki",
+            "5. ZK ->: Mae-Mawashi-Geri + Ushiro-Geri",
+            "6. KB <->: Yoko-Geri-Keage (Beistellschritt) + Kekomi (2x l. + 2x r+.)",
+            "7. ZK ->: Ura-Mawashi-Geri (alternativ Ashi-Barai, hinteres Bein) + Gyaku-Zuki"
+        ],
+        "currentCombination": 0
+    },
+    {
+        "title": "2. Kyu (Braun II)",
+        "combinations": [
+            "1. ZK ->: Uraken-Uchi + Gyaku-Zuki",
+            "2. NA <-: Shuto-Uke + Mae-Ashi-Geri + ZK Gyaku-Tate-Nukite",
+            "3. ZK <-: Soto-Ude-Uke + KB Yoko-Empi + Yoko-Uraken-Uchi + ZK Gyaku-Zuki",
+            "4. SD <-: Uchi-Ude-Uke + ZK Gyaku-Zuki",
+            "5. ZK ->: Ashi-Barai (vorderes Bein) + Uraken-Uchi vor Mawashi-Geri + Gyaku-Zuki (Chudan Kamae)",
+            "6. ZK ->: Kizami-Zuki (YA) + Gyaku-Zuki + vor Ura-Mawashi (alternativ Ashi-Barai + Gyaku-Zuki) (Chudan Kamae)",
+            "7. ZK ->: Kizami-Zuki (YA) + Ushiro-Geri + Gyaku-Zuki (Chudan Kamae)"
+        ],
+        "currentCombination": 0
+    }
+]
+
 let selectedIndex = -1  // all
 let editBelts = false
 
